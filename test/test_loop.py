83f9158b7567790a8c07f904378770b6c71666d9
Omar <gomar@dmacc.edu>
2019-09-22 10:44:24 -0700

    first test passed

--- test/test_loop.py
+++ test/test_loop.py
@@ -1,10 +1,12 @@
 import unittest
 from input_loops import average_input_scores
+import unittest.mock as result
+
 
 class MyTestCase(unittest.TestCase):
-        def test_average(self):
-            self.assertEqual(90, average_input_scores.average( list ))
+    def test_average(self):
+        with result.patch('builtins.input', side_effect=[90, 95, 85]):
+            self.assertEquals(90, average_input_scores.average(list))
 
-
-if __name__ == '__main__':
-    unittest.main()
+    if __name__ == '__main__':
+        unittest.main()
