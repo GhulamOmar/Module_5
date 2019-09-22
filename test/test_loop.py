e64e89cd752a754b3e1fa41da49959c808370074
Omar <gomar@dmacc.edu>
2019-09-22 08:24:41 -0700

    initial test

---
+++ test/test_loop.py
@@ -0,0 +1,10 @@
+import unittest
+from input_loops import average_input_scores
+
+class MyTestCase(unittest.TestCase):
+        def test_average(self):
+            self.assertEqual(90, average_input_scores.average( list ))
+
+
+if __name__ == '__main__':
+    unittest.main()
